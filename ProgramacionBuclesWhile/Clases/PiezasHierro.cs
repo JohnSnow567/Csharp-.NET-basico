b4424e213a121a3732759e42d8619d28c1dc5d0f
Jonathan de Jesus Martinez Sanchez <20231417@itla.edu.do>
2024-08-17 17:14:58 -0400

    Correccion de PiezasHierro

--- ProgramacionBuclesWhile/Clases/PiezasHierro.cs
+++ ProgramacionBuclesWhile/Clases/PiezasHierro.cs
@@ -12,38 +12,45 @@ namespace ProgramacionBuclesWhile.Clases
         {
             try
             {
+                int numPiezas = 0;
+                decimal longitudMinima = 0;
+                decimal longitudMaxima = 0;
+                decimal longitudPieza = 0;
+                int contadorAptas = 0;
+                int i = 0;
+                
                 //Pedimos al usuario que ingrese el numero total de piezas
                 Console.WriteLine("Ingrese el número total de piezas: ");
-                int numPiezas = int.Parse(Console.ReadLine());
+                numPiezas = int.Parse(Console.ReadLine());
 
                 //Pedimos al usuario la longitud minima de las piezas
                 Console.WriteLine("Ingrese la longitud mínima del rango (en cm): ");
-                double longitudMinima = double.Parse(Console.ReadLine());
+                longitudMinima = decimal.Parse(Console.ReadLine());
 
                //Pedimos al usuario la longitud maxima de las piezas
                 Console.WriteLine("Ingrese la longitud máxima del rango (en cm): ");
-                double longitudMaxima = double.Parse(Console.ReadLine());
+                longitudMaxima = decimal.Parse(Console.ReadLine());
 
-               //Creamos una variable para almacenar las piezas aptas
-                int contadorAptas = 0;
 
-                //Creamos un bucle for para iterar sobre el numero de piezas que quiere el usuario
-                for (int i = 0; i < numPiezas; i++)
+                while ( i < numPiezas)
                 {
-                    
                     //Pedimos al usuario la longitud de la pieza y la almacenamos en una variable
                     Console.WriteLine($"Ingrese la longitud de la pieza {i + 1} (en cm): ");
-                    double longitudPieza = double.Parse(Console.ReadLine());
+                    longitudPieza = decimal.Parse(Console.ReadLine());
 
                     //Si la longitud de la pieza es mayor que el minimo y menor que el maximo, incrementamos el contador
                     if (longitudPieza >= longitudMinima && longitudPieza <= longitudMaxima)
                     {
                         contadorAptas++;
+                        Console.WriteLine($"Pieza {i + 1} es apta"); // Imprimir la pieza apta directamente
                     }
+
+                    i++;
                 }
 
                 //Desplegamos en pantalla el numero de piezas que son aptas
                 Console.WriteLine($"Número de piezas aptas para fabricar perfiles: {contadorAptas}");
+
             }
 
             catch (Exception ex)
